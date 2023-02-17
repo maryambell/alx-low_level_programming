333a3d1accb017f6a4eaea75ebebd318bca8d92b
maryambell <maryan.ahmedhaji@gmail.com>
2023-02-17 12:16:53 -0800

    100-print_comb3

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -10,20 +10,23 @@ int main(void)
 {
 int n;
 int i;
-for (n = 0 ; n < 9 ; n++)
+int h;
+
+for (h = 0 ; h != 8 ; h++)
 {
+for (n = h + 1 ; n != 9 ; n++)
+                {
 i = n + 1;
 do {
+putchar('0' + h);
 putchar('0' + n);
 putchar('0' + i);
-if (n < 8)
+if (h != 7)
 {
 putchar(',');
 putchar(32);
 }
 i++;
-} while (i < 10);
+} while (i != 10);
 }
-putchar('\n');
-return (0);
-}
+
