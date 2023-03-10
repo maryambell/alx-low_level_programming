4e2c05aaf72faaa348c35fa5fb969c74679bd0d5
maryambell <maryan.ahmedhaji@gmail.com>
2023-03-10 07:02:52 -0800

    libmy.a

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -1,14 +1,14 @@
 #include "main.h"
-
 /**
- * _puts - prints a string, followed by a new line,
- * @str: pointer to the string to print
- * Return: void
+ * _puts - print a string
+ * @str: string
  */
+
 void _puts(char *str)
 {
-int i = 0;
-while (str[i])
+int i;
+i = 0;
+while (str[i] != '\0')
 {
 _putchar(str[i]);
 i++;
