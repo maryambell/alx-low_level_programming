435a47d4b51e6b6f85775d5a1601df3028f6589c
maryambell <maryan.ahmedhaji@gmail.com>
2023-04-04 06:59:41 -0700

    4-free_listint.c

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -3,17 +3,14 @@
 /**
  * free_listint - function that frees a listint_t list
  * @head: head of a list
- *
- * Return: Nothing
  */
 void free_listint(listint_t *head)
 {
 listint_t *tmp;
-while
-(head != NULL);
+while (head)
 {
-tmp = head;
-head = head->next;
-free(tmp);
+tmp = head->next;
+free(head);
+head = tmp;
 }
 }
