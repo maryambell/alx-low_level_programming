c01055b14d5b6e2a3eec9483cd32845217d06c31
maryambell <maryan.ahmedhaji@gmail.com>
2023-04-06 04:23:17 -0700

    0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -10,12 +10,12 @@
 unsigned int binary_to_uint(const char *b)
 {
 unsigned int x;
-int len, base_2;
+int len, base_two;
 if (!b)
 return (0);
 x = 0;
 for (len = 0; b[len] != '\0'; len++)
-for (len --, base_2 = 1; len >= 0; len--, base_2 *= 2)
+for (len --, base_two = 1; len >= 0; len--, base_two *= 2)
 {
 if (b[len] != '0' && b[len] != '1')
 {
@@ -23,7 +23,7 @@ return (0);
 }
 if (b[len] & 1)
 {
-x += base_2;
+x += base_two;
 }
 }
 return (x);
